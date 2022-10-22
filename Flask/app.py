a4234ff2d2bf4d4659f3f0819b8829ec41d2158c
Shem Mwanza <shem.mwanza@strathmore.edu>
2022-10-23 01:02:44 +0300

    add lion to DB template

--- Flask/app.py
+++ Flask/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/addlion")
+def add_lion():
+	return render_template("add_lion.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
