a16f6461051c223e6f4074af5892ec7b6db68457
kalvin-osoro <62253122+kalvin-osoro@users.noreply.github.com>
2022-11-05 23:14:03 +0300

    updates

--- Flask/app.py
+++ Flask/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request,  redirect, url_for
 # from werkzeug import secure_filename
+
+
 
 app = Flask(__name__)
 
@@ -8,19 +11,26 @@ app = Flask(__name__)
 # def index():
 # 	return render_template("index.html")
 
+
+path = r"Flask\dataset"
+assert os.path.isfile(path)
+with open(path, "r") as f:
+    pass
+
+
 @app.route("/", methods=['GET','POST'])
 def add_lion():
 	if request.method == "POST":
 		# lion_name = request.form['lion_name']
 		# lion_pride = request.form['lion_pride']
 		# dob = request.form['dob']
-		imagefile = request.files['imagefile']
-		image_path = "./images/" + imagefile.filename
+		imagefile = request.files['imagefile']		
+		image_path = "/" + imagefile.filename
 		imagefile.save(image_path)
 
-		return "upload successful " 
+		return "upload successful "+ imagefile 
 		 
-		# return "Your name is "+lion_name +"/n pride " + lion_pride + dob+ imagefile
+		# return "Your name is "+lion_name +"/n pride " + lion_pride + dob
 
 	return render_template("add_lion.html")
 	
@@ -42,31 +52,3 @@ if __name__ == '__main__':
 	app.run(debug=True)
 
 
-
-
-# from flask import Flask, render_template, request
-
-# app = Flask(__name__)
-
-# @app.route("/", methods=['GET'])
-# @app.route("/index")
-# def index():
-# 	return render_template("index.html")
-
-# @app.route("/addlion")
-# def add_lion():
-	# lion_name = request.files['name']
-	# lion_pride = request.files['lion_pride']
-	# imagefile = request.files['imagefile']
-	# image_path = "./images/" + imagefile.filename
-	# imagefile.save(image_path)
-
-# 	return render_template("add_lion.html")
-
-
-# @app.route("/findlion", methods=['GET'])
-# def find_lion():
-# 	return render_template("find_lion.html")
-
-# if __name__ == '__main__':	
-# 	app.run(debug=True)
