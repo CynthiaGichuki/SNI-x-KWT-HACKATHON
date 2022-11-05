c670a0746282e967a1e29895fd18c8da1a2660ad
Shem Mwanza <shem.mwanza@strathmore.edu>
2022-11-05 23:48:19 +0300

    Save image to path

--- Flask/app.py
+++ Flask/app.py
@@ -6,24 +6,25 @@ UPLOAD_FOLDER = 'image/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-@app.route("/")
+
+# @app.route("/")
 @app.route("/index")
 def index():
 	return render_template("index.html")
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/")
 def add_lion():
-	if request.method == "POST":
-		# lion_name = request.form['lion_name']
-		# lion_pride = request.form['lion_pride']
-		# dob = request.form['dob']
-		imagefile = request.files['imagefile']		
-		image_path = "/" + imagefile.filename
-		imagefile.save(image_path)
+	# if request.method == "POST":
+	# 	# lion_name = request.form['lion_name']
+	# 	# lion_pride = request.form['lion_pride']
+	# 	# dob = request.form['dob']
+	# 	imagefile = request.files['imagefile']		
+	# 	image_path = "/" + imagefile.filename
+	# 	imagefile.save(image_path)
 
-		return "upload successful "+ imagefile 
+	# 	return "upload successful "+ imagefile 
 		 
-		# return "Your name is "+lion_name +"/n pride " + lion_pride + dob
+	# 	# return "Your name is "+lion_name +"/n pride " + lion_pride + dob
 
 	return render_template("add_lion.html")
 
@@ -34,7 +35,7 @@ def find_lion():
 @app.route('/results', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
+      f = request.files['imagefile']
       f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
       return 'Image saved at ' + UPLOAD_FOLDER + 'folder'
 
